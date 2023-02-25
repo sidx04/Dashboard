b6e24e34f4016577390c927b340651b8e1ced25b
Onion-Man <siddpal04@gmail.com>
2023-02-25 21:54:03 +0530

    updated page structure

---
+++ client/src/pages/index.ts
@@ -0,0 +1,21 @@
+import agentProfile from "./agent-profile";
+import agent from "./agent";
+import allProperties from "./all-properties";
+import createProperty from "./create-property";
+import editProperty from "./edit-property";
+import home from "./home";
+import myProfile from "./my-profile";
+import propertyDetails from "./property-details";
+import { Login } from "./login";
+
+export default {
+    agent,
+    agentProfile,
+    allProperties,
+    createProperty,
+    editProperty,
+    home,
+    myProfile,
+    Login,
+    propertyDetails
+};
