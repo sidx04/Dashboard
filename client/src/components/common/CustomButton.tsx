4e1a61bc4f9c09f4bd9cf0b6c12b1dfdbf2cb8a4
Onion-Man <siddpal04@gmail.com>
2023-03-08 12:01:35 +0530

    created all properties component

--- client/src/components/common/CustomButton.tsx
+++ client/src/components/common/CustomButton.tsx
@@ -1,8 +1,37 @@
+import { Button } from '@pankod/refine-mui'
+import { CustomButtonProps } from 'interfaces/common'
 import React from 'react'
 
-const CustomButton = () => {
+const CustomButton = ({
+   type,
+   title, 
+   backgroundColor, 
+   color, 
+   fullWidth, 
+   icon, 
+   handleClick}: CustomButtonProps) => {
   return (
-    <div>CustomButton</div>
+    <Button
+      sx={{
+        flex: fullWidth ? 1 : 'unset',
+        padding: '10px 15px',
+        width: fullWidth ? '100%' : 'fit-content',
+        minWidth: 130,
+        backgroundColor,
+        color,
+        fontSize: 16,
+        fontWeight: 600,
+        gap: '10px',
+        textTransform: 'capitalize',
+        '&:hover':{
+          backgroundColor: '#4a47e8',
+        }
+      }}
+      onClick={ handleClick }
+    >
+      {icon}
+      {title}
+    </Button>
   )
 }
 
